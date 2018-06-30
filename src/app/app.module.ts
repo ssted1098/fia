9cf3cd8130ddde12fc5fa4f3dc9fe3831b1b3586
ssted1098 <ssted1098@yahoo.co.jp>
2018-06-30 10:45:21 +0900

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HomeComponent } from './home/home.component';
+import { AppNavComponent } from './app-nav/app-nav.component';
+import { AppRoutingModule } from './/app-routing.module';
+import { ProcessListComponent } from './process-list/process-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NotFoundComponent,
+    HomeComponent,
+    AppNavComponent,
+    ProcessListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
